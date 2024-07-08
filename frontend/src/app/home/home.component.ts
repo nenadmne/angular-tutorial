24311a6bbdd28a20a349d3727f978a34f1a110fa
nenadmne <cosovicnenad14@gmail.com>
2024-07-08 21:51:11 +0200

    Added pagination

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -3,12 +3,13 @@ import { ProductsService } from '../services/products.service';
 import { Product, Products } from '../../types';
 import { ProductComponent } from '../components/product/product.component';
 import { CommonModule } from '@angular/common';
+import { PaginatorModule } from 'primeng/paginator';
 
 // ComonModule needed for activating for loop
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductComponent, CommonModule],
+  imports: [ProductComponent, CommonModule, PaginatorModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
@@ -16,21 +17,32 @@ export class HomeComponent {
   constructor(private productService: ProductsService) {}
 
   products: Product[] = [];
-
-  // Sending url and option parameters to Api Service trough Product service
-  // Option parameters are query parameters (page and per page) that will mutate the url and extract needed products
-  ngOnInit() {
-    this.productService
-      .getProducts('http://localhost:3000/clothes', {
-        page: 0,
-        perPage: 5,
-      })
-      .subscribe((products: Products) => {
-        this.products = products.items;
-      });
-  }
+  totalRecords: number = 0;
+  rows: number = 5;
 
   onProductOutput(product: Product) {
     console.log(product, 'Output');
+  }
+
+  // Sending url and option parameters to Api Service trough Product service
+  // Option parameters are query parameters (page and per page) that will mutate the url and extract needed products
+  fetchProducts(page: number, perPage: number) {
+    this.productService
+      .getProducts('http://localhost:3000/clothes', {
+        page,
+        perPage,
+      })
+      .subscribe((products: Products) => {
+        this.products = products.items;
+        this.totalRecords = products.total;
+      });
+  }
+
+  ngOnInit() {
+    this.fetchProducts(0, this.rows);
+  }
+
+  onPageChange(event: any) {
+    this.fetchProducts(event.page, event.rows);
   }
 }
